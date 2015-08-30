ff0cb68696124d95ea0c9d78de6dcdb90dbae189
Grzegorz Świrski <grzegorz@swirski.name>
2015-08-30 09:17:55 -0700

    handle ssl connections

--- server/lib/server.ex
+++ server/lib/server.ex
@@ -1,32 +1,45 @@
 defmodule Server do
-  def accept(port) do
-    opts = [:binary, packet: :line, active: false, reuseaddr: true]
-    {:ok, socket} = :gen_tcp.listen(port, opts)
+  def ac() do
+    port = 24948
+    opts = [
+      certfile: "server.crt",
+      keyfile: "server.key",
+      reuseaddr: true,
+      versions: [:"tlsv1.2"],
+      active: false
+    ]
+    :ssl.start()
+    {:ok, socket} = :ssl.listen(port, opts)
+    IO.inspect(socket)
     IO.puts "Accepting connections on port #{port}"
     loop_acceptor(socket)
   end
 
+  def st() do
+    :ssl.stop()
+  end
+
   defp loop_acceptor(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, client} = :ssl.transport_accept(socket)
+    ok = :ssl.ssl_accept(client)
+    IO.inspect(ok)
     serve(client)
     loop_acceptor(socket)
   end
 
   defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
-
+    data = read_line(socket)
+    write_line('s16 c1 ok\nwat\n', socket)
     serve(socket)
   end
 
   defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-    IO.puts(data)
+    {:ok, data} = :ssl.recv(socket, 0)
+    IO.inspect(data)
     data
   end
 
   defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
+    :ssl.send(socket, line)
   end
 end
